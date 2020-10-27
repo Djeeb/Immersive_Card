10ae8b2f78cfdea09818346f7dceebc7901bcd46
Olivier JEAN-BAPTISTE <ojeanbaptiste75@gmail.com>
2020-10-27 21:45:17 +0100

    finished card 3d effect

--- app.js
+++ app.js
@@ -2,6 +2,13 @@
 const card = document.querySelector(".card");
 const container = document.querySelector(".container");
 
+//Items
+const title = document.querySelector(".title");
+const dancer = document.querySelector(".dancer img");
+const purchase = document.querySelector(".purchase button");
+const description = document.querySelector(".info h3");
+const dates = document.querySelector(".dates");
+
 //Moving Animation Event
 container.addEventListener("mousemove", (e) => {
   let xAxis = (window.innerWidth / 2 - e.pageX) / 25;
@@ -12,10 +19,22 @@ container.addEventListener("mousemove", (e) => {
 //Animate In
 container.addEventListener("mouseenter", (e) => {
   card.style.transition = "none";
+  //Popout
+  title.style.transform = "translateZ(75px)";
+  dancer.style.transform = "translateZ(120px) rotateZ(-15deg)";
+  description.style.transform = "translateZ(65px)";
+  dates.style.transform = "translateZ(50px)";
+  purchase.style.transform = "translateZ(40px)";
 });
 
 //Animate Out
 container.addEventListener("mouseleave", (e) => {
   card.style.transition = "all 0.5s ease";
   card.style.transform = "rotateY(0deg) rotateX(0deg)";
+  //Popback
+  title.style.transform = "translateZ(0px)";
+  dancer.style.transform = "translateZ(0px) rotateZ(0deg)";
+  description.style.transform = "translateZ(0px)";
+  dates.style.transform = "translateZ(0px)";
+  purchase.style.transform = "translateZ(0px)";
 });
